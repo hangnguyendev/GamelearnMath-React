e0bcda60749d22dbac6b8ae7e72a5a4a77bee9eb
PN\hang.nguyen <hang.nguyen@student.passerellesnumeriques.org>
2018-06-08 18:01:39 +0700

    Fix typo in introduction to user guide

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '../src/Component/Button';
-// import { Provider as AlertProvider } from 'react-alert';
 
 class App extends React.Component {
   constructor() {
@@ -11,66 +10,186 @@ class App extends React.Component {
       random: 4,
       choosenumber: [],
       totalNumber: 4,
-      choosednumber: []
+      choosednumber: [],
+      countReset: 0,
+      time: {},
+      seconds: 30,
+      start_game: false,
+      reTime: 0,
+      end_game: true
     };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
 
+    this.beginGame = this.beginGame.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.removeButton = this.removeButton.bind(this);
     this.sumOfChooseNumeber = this.sumOfChooseNumeber.bind(this);
-    this.handleClick();
+
+    this.restart = this.restart.bind(this);
+
+    this.resetRandom = this.resetRandom.bind(this);
   }
 
-  printStar = () => {
+  startGame() {
+    if (this.state.start_game === false) {
+      return (
+        <div className="start_game">
+          <button onClick={this.beginGame}> START GAME </button>
+        </div>
+      );
+    }
+  }
+
+  endGame() {
+    if (this.state.end_game === false) {
+      return (
+        <div className="start_game">
+          <p>
+            <h1>You lose</h1>
+          </p>
+          <button onClick={this.restart}> RESTART GAME </button>
+        </div>
+      );
+    }
+  }
+
+  restart() {
+    this.setState({ start_game: true });
+    this.setState({ end_game: true });
+    clearInterval(this.timer);
+    this.setState({ choosednumber: [] });
+    this.setState({ choosenumber: [] });
+    this.timer = 0;
+    this.setState({ seconds: 5 });
+    this.startTimer();
+  }
+
+  secondsToTime(secs) {
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      h: hours,
+      m: minutes,
+      s: seconds
+    };
+    return obj;
+  }
+
+  componentWillMount() {
+    let timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+  }
+
+  startTimer() {
+    if (this.timer === 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    let seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds
+    });
+
+    if (this.state.seconds === 0) {
+      clearInterval(this.timer);
+      this.setState({ end_game: false });
+    }
+  }
+
+  printStar() {
     let stars = [];
     for (let j = 0; j < this.state.random; j++) {
       stars.push(<i className="fa fa-star fa-spin" />);
     }
     return stars;
-  };
+  }
 
   handleClick() {
     this.setState({
-      choosednumber: [...this.state.choosednumber, this.state.choosenumber]
+      choosednumber: [...this.state.choosednumber, ...this.state.choosenumber]
     });
     if (this.state.choosenumber && this.state.choosenumber.length > 0) {
       const reducer = (accumulator, currentValue) => accumulator + currentValue;
       const total = this.state.choosenumber.reduce(reducer);
       this.setState({ totalNumber: total });
-      console.log(total);
+
       if (this.state.random === total) {
-        const random = this.state.random;
         const x = Math.floor(Math.random() * 10 + 1);
         this.setState({ random: x });
         this.setState({ choosenumber: [] });
+
+        clearInterval(this.timer);
+
+        this.timer = 0;
+        this.setState({ seconds: 5 });
+
+        this.startTimer();
       } else {
-        alert('you lose');
+        clearInterval(this.timer);
+        this.setState({ end_game: false });
       }
     } else {
       const totalNumber = 0;
       this.setState({ totalNumber: totalNumber });
-      // alert("you lose");
+      alert("you didn't choose the number");
     }
+    return 0;
   }
 
   sumOfChooseNumeber() {}
 
-  onClickBtn = e => {
+  onClickBtn(e) {
+    if (!e.target.value) return;
     let number = parseInt(e.target.value);
     this.setState({ choosenumber: [...this.state.choosenumber, number] });
 
     return 0;
-  };
+  }
+
+  beginGame() {
+    this.setState({ start_game: true });
+    return true;
+  }
 
   removeButton = e => {
-    let number = e.target.value;
-    let a = this.state.choosenumber.indexOf(number);
-    this.state.choosenumber.splice(a, 1);
-    this.state.choosednumber.splice(a, 1);
-    this.setState({ choosenumber: this.state.choosenumber });
-    this.setState({
-      choosednumber: [...this.state.choosednumber, this.state.choosenumber]
-    });
+    if (!e.target.value) return;
+    const { choosenumber } = this.state;
+
+    choosenumber.splice(choosenumber.indexOf(Number(e.target.value)), 1);
+
+    console.log(choosenumber, ' rcountReset');
+    this.setState({ choosenumber: choosenumber });
+
+    return 0;
   };
+
+  resetRandom() {
+    if (this.state.countReset === 5) {
+      alert('you lose');
+    } else {
+      const x = Math.floor(Math.random() * 10 + 1);
+      this.setState({ random: x });
+      this.setState({ countReset: this.state.countReset + 1 });
+      clearInterval(this.timer);
+      // this.setState({ choosednumber: [] });
+      // this.setState({ choosenumber: [] });
+      this.timer = 0;
+      this.setState({ seconds: 5 });
+      this.startTimer();
+    }
+    return 0;
+  }
 
   renderChooseButton() {
     return (
@@ -92,37 +211,49 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state.totalNumber, 'total number');
-    console.log(this.state.random, 'rand');
-    console.log(this.state.choosednumber, ' number');
     return (
-      <div>
-        <div className="left-content" id="left">
-          {this.printStar(this.state.random)}
-        </div>
+      <div className="content">
+        {this.startGame()}
+        {this.state.start_game &&
+          this.state.end_game && (
+            <div>
+              {this.startTimer()}
+              <button className="timer">Timer: {this.state.time.s}</button>
+              <div className="left-content" id="left">
+                {this.printStar(this.state.random)}
+              </div>
 
-        <div className="right-content" id="right">
-          {this.renderChooseButton()}
-        </div>
+              <div className="right-content" id="right">
+                {this.renderChooseButton()}
+              </div>
 
-        <div className="center">
-          <button className="btn default" onClick={this.handleClick}>
-            {' '}
-            ={' '}
-          </button>
-          <button className="btn default"> Reset </button>
-        </div>
-        <div className="button-below">
-          {this.state.numbers.map((item, i) => (
-            <Button
-              className="btn"
-              value={item}
-              onClick={e => this.onClickBtn(e)}
-              name={item}
-              disableBtn={this.state.choosenumber.indexOf(item) !== -1}
-            />
-          ))}
-        </div>
+              <div className="center">
+                <button className="btn default" onClick={this.handleClick}>
+                  {' '}
+                  ={' '}
+                </button>
+                <button className="btn default" onClick={this.resetRandom}>
+                  {' '}
+                  Reset{' '}
+                </button>
+              </div>
+              <div className="button-below">
+                {this.state.numbers.map((item, i) => (
+                  <Button
+                    className="btn"
+                    value={item}
+                    onClick={e => this.onClickBtn(e)}
+                    name={item}
+                    disableBtn={
+                      this.state.choosenumber.indexOf(item) !== -1 ||
+                      this.state.choosednumber.indexOf(item) !== -1
+                    }
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+        {this.endGame()}
       </div>
     );
   }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Button from '../src/Component/Button';
-import Butonequal from './App';
-import Star from './App';
 
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import registerServiceWorker from './registerServiceWorker';
